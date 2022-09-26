bd30250dfa17473e1762bb78473652179c6d2dbd
radiancegeorge <radiancegeorge@outlook.com>
2022-09-26 02:01:53 +0100

    added paystack hooks

--- controllers/orders/index.js
+++ controllers/orders/index.js
@@ -1,7 +1,9 @@
 const asyncHandler = require("express-async-handler");
 const { validationResult } = require("express-validator");
 const Orders = require("../../helpers/orders");
-
+const crypto = require("crypto");
+const db = require("../../models");
+const { paystackSecretKey } = process.env;
 const createOrder = asyncHandler(async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty())
@@ -14,6 +16,35 @@ const createOrder = asyncHandler(async (req, res) => {
   res.send(orders);
 });
 
+const handlePayment = asyncHandler(async (req, res) => {
+  //validate event
+  const hash = crypto
+    .createHmac("sha512", paystackSecretKey)
+    .update(JSON.stringify(req.body))
+    .digest("hex");
+  if (hash == req.headers["x-paystack-signature"]) {
+    const { data } = req.body;
+    // Do something with event
+    if (data.status === "success") {
+      //get order id from metadata
+      const { orderId } = data.metadata;
+      await db.orders.update(
+        {
+          isPaid: true,
+        },
+        {
+          where: {
+            id: orderId,
+          },
+        }
+      );
+      //send mail
+    }
+  }
+
+  res.send();
+});
 module.exports = {
   createOrder,
+  handlePayment,
 };
--- routes/order/index.js
+++ routes/order/index.js
@@ -1,8 +1,9 @@
-const { createOrder } = require("../../controllers/orders");
+const { createOrder, handlePayment } = require("../../controllers/orders");
 const { createOrderValidations } = require("./validations");
 
 const orders = require("express").Router();
 
 orders.route("/order").post(createOrderValidations, createOrder);
+orders.route("/order/paystack").post(handlePayment);
 
 module.exports = orders;
